a8b7bd3e46f91adbd492a8ad0073ffacf547cf64
Alyax0 <alessandroaxespinosanavarro@gmail.com>
2026-08-13 17:22:15 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LiveTracker from "./LiveTracker.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LiveTracker />
+  </React.StrictMode>
+);
